6828a2fede577ce7c408e78211229625f593893d
johann <johanjoseph100@gmail.com>
2024-02-10 15:30:01 -0600

    install libraries

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCodeTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveCodeTesting.java
@@ -4,6 +4,8 @@ import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gam
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad2;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 
+import com.arcrobotics.ftclib.hardware.motors.CRServo;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -12,6 +14,7 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.hardware.Servo;
+
 
 @SuppressWarnings("unused")
 @TeleOp(name = "DriveCodeTesting", group = "Linear OpMode")
@@ -39,25 +42,38 @@ public class DriveCodeTesting extends LinearOpMode{
 //                hardware.topDifferential.setPower(0);
 //                hardware.bottomDifferential.setPower(0);
 //            }
-            if (gamepad2.left_stick_y > 0.1)
+//            if (gamepad2.a)
+//            {
+//                hardware.bottomDifferential.setPower(1);
+//            }
+//            else if (gamepad2.b)
+//            {
+//                hardware.bottomDifferential.setPower(0);
+//            }
+//            else
+//            {
+//                hardware.topDifferential.setPower(0.5);
+//            }
+//
+//            if (gamepad2.x)
+//            {
+//                hardware.bottomDifferential.setPower(1);
+//            }
+//            else if (gamepad2.y)
+//            {
+//                hardware.bottomDifferential.setPower(0);
+//            }
+//            else
+//            {
+//                hardware.bottomDifferential.setPower(0.5);
+//            }
+            if (gamepad2.a)
             {
-                hardware.topDifferential.setDirection(DcMotorSimple.Direction.FORWARD);
-                hardware.topDifferential.setPower(0.5);
+                hardware.topDifferential.setPower(0);
             }
-            else if (gamepad2.left_stick_y < -0.1)
+            else
             {
-                hardware.topDifferential.setDirection(DcMotorSimple.Direction.REVERSE);
-                hardware.topDifferential.setPower(0.5);
-            }
-            if (gamepad2.right_stick_y > 0.1)
-            {
-                hardware.bottomDifferential.setDirection(DcMotorSimple.Direction.FORWARD);
-                hardware.bottomDifferential.setPower(0.5);
-            }
-            else if (gamepad2.right_stick_y < -0.1)
-            {
-                hardware.bottomDifferential.setDirection(DcMotorSimple.Direction.REVERSE);
-                hardware.bottomDifferential.setPower(0.5);;
+                hardware.topDifferential.setPower(0);
             }
         }
     }
